9d1ff15fed011178968f7e42d7e4043a86570990
s-ude <ude@mediatis.de>
2025-05-26 23:00:02 +0200

    [TASK] TYPO3 13 Update (MFR-136) (#3)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framework - Distributor - SalesForce Web-To-Lead',
+    'title' => 'Anyrel - Distributor - SalesForce Web-To-Lead',
     'description' => 'Send form data to SalesForce via form Web-To-Lead API',
     'category' => 'be',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '2.1.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'dmf_distributor_request' => '2.0.0-2.99.99',
+            'typo3' => '12.4.0-13.4.99',
+            'dmf_distributor_request' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
         ],
